d71bfad2a02b1bf9271c68209d576487640882e6
Ahmed Elsherif <s-aelsherif500@zewailcity.edu.eg>
2021-02-10 01:09:35 +0200

    feat: mongo env variable

--- db-connect.js
+++ db-connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/covid19', {
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/covid19', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify:false,
