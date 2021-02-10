013c7e3d62c5dfc4575dee907da8431ecdc18765
Ahmed Elsherif <s-aelsherif500@zewailcity.edu.eg>
2021-02-10 02:05:14 +0200

    feat: port env variable

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Something broke!')
 })
 const port = 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is listening on: http://localhost:${port}`)
 })
 
